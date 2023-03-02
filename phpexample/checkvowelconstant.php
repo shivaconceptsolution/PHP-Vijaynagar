35ede2a01a6ba08141d00a98d77def64644fafa0
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2023-03-02 17:09:11 +0530

    Add files via upload

---
+++ phpexample/checkvowelconstant.php
@@ -0,0 +1,17 @@
+<?php
+$ch = 'b';
+switch($ch)
+{
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        echo "Vowel";
+        break;
+    default:
+       echo "Consonent";
+       break;
+}
+
+?>
